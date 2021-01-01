5ad5450e9c936e5283126d792f80c75f4bea004a
yzhu <891085309@qq.com>
2021-01-01 23:54:47 +0800

    [TEMP]

--- init/main.c
+++ init/main.c
@@ -1,10 +1,11 @@
-#include "types.h"
-#include "riscv.h"
-#include "memlayout.h"
-#include "kalloc.h"
-#include "vm.h"
-#include "task.h"
-#include "printv.h"
+#include <types.h>
+#include <riscv.h>
+#include <memlayout.h>
+#include <kalloc.h>
+#include <vm.h>
+#include <task.h>
+#include <log.h>
+#include <core/class.h>
 
 extern void kernelvec();
 // set up to take exceptions and traps while in the kernel.
@@ -15,19 +16,19 @@ void trapinithart(void)
 
 void hello()
 {
-    while (1)
-    {
-        schedule();
-    }
+    LOGI("hello");
+    while (1);
 }
 
 void main()
 {
-    printv("main\r\n");
+    LOGI("main");
     kinit();
     trapinithart();
     kvminit();
     kvminithart();
+
+    do_init_class();
     task_init();
     task_create("test0", hello);
 
