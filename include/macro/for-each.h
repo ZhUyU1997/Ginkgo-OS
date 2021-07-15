e7dcea6db00cd918545c177d460044e0341617fc
yzhu <891085309@qq.com>
2021-07-16 01:57:47 +0800

    Filter empty args

--- include/macro/for-each.h
+++ include/macro/for-each.h
@@ -3,10 +3,13 @@
 #include <macro/base.h>
 #include <macro/eval.h>
 
-#define _FOR_EACH(macro, x, ...)           \
-    CAT(_FOR_EACH_, IS_EMPTY(__VA_ARGS__)) \
-    (macro, x, __VA_ARGS__)
-#define _FOR_EACH_0(macro, x, ...) macro(x) DEFER(_FOR_EACH_I)()(macro, __VA_ARGS__)
-#define _FOR_EACH_1(macro, x, ...) macro(x)
+#define _FOR_EACH(macro, x, ...)     \
+    CAT(_FOR_EACH_CUR_, IS_EMPTY(x))(macro, x) CAT(_FOR_EACH_NEXT_, IS_EMPTY(__VA_ARGS__))(macro, __VA_ARGS__)
+
+#define _FOR_EACH_NEXT_0(macro, ...) DEFER(_FOR_EACH_I)()(macro, __VA_ARGS__)
+#define _FOR_EACH_NEXT_1(macro, ...)
+#define _FOR_EACH_CUR_0(macro, x) macro(x)
+#define _FOR_EACH_CUR_1(macro, x)
+
 #define _FOR_EACH_I() _FOR_EACH
 #define FOR_EACH(macro, ...) EVAL(_FOR_EACH(macro, __VA_ARGS__))
--- include/printv.h
+++ include/printv.h
@@ -15,28 +15,19 @@
 #define PRINT_TEMPLATE2(fmt, type) \
     static inline void print_##fmt(type value)
 
-#define PRINT_0(value) _Generic(      \
+#define PRINT(value) _Generic(        \
     (value),                          \
     PRINT_FUNC(s, char *),            \
-    PRINT_FUNC(s, const char *),            \
-    PRINT_FUNC(d, short),               \
-    PRINT_FUNC(u, unsigned short),      \
+    PRINT_FUNC(s, const char *),      \
+    PRINT_FUNC(d, short),             \
+    PRINT_FUNC(u, unsigned short),    \
     PRINT_FUNC(d, int),               \
     PRINT_FUNC(u, unsigned int),      \
     PRINT_FUNC(p, long int),          \
     PRINT_FUNC(x, unsigned long int), \
     PRINT_FUNC(p, default))(value);
 
-// #define PRINT_0(x)  x;
-#define PRINT_1(x)  
-#define __PRINTV(x) CAT(PRINT_,IS_EMPTY(x))(x)
-#define _PRINTV_0(...) ({ FOR_EACH(__PRINTV, ##__VA_ARGS__) })
-#define _PRINTV_1(...)
-
-#define _PRINTV(...)                      \
-    CAT(_PRINTV_, IS_EMPTY(__VA_ARGS__)) \
-    (__VA_ARGS__)
-#define printv(...)  _PRINTV(__VA_ARGS__) 
+#define printv(...) ({ FOR_EACH(PRINT, ##__VA_ARGS__) })
 #define $(x) , x,
 
 PRINT_TEMPLATE(p, const void *);
