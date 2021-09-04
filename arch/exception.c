e66835390bd6377bb9d3ac33fa6c89c6f3a2bd81
yzhu <891085309@qq.com>
2021-09-05 07:00:17 +0800

    Better exception print

--- arch/exception.c
+++ arch/exception.c
@@ -36,48 +36,48 @@ void do_timer_init()
     csr_set(mie, MIE_MTIE);                 // enable machine-mode timer interrupts.
 }
 
-static const char * get_scause_info(uint64 scause)
+static const char *get_scause_info(uint64 scause)
 {
     if (scause == 0)
         return ("Instruction address misaligned");
     else if (scause == 1)
         return ("Instruction access fault");
     else if (scause == 2)
-        return("Illegal instruction");
+        return ("Illegal instruction");
     else if (scause == 3)
-        return("Breakpoint");
+        return ("Breakpoint");
     else if (scause == 4)
-        return("Load address misaligned");
+        return ("Load address misaligned");
     else if (scause == 5)
-        return("Load access fault");
+        return ("Load access fault");
     else if (scause == 6)
-        return("Store/AMO address misaligned");
+        return ("Store/AMO address misaligned");
     else if (scause == 7)
-        return("Store/AMO access fault");
+        return ("Store/AMO access fault");
     else if (scause == 8)
-        return("Environment call from U-mode");
+        return ("Environment call from U-mode");
     else if (scause == 9)
-        return("Environment call from S-mode");
+        return ("Environment call from S-mode");
     else if (scause == 10 || scause == 11)
-        return("Reserved for future standard use");
+        return ("Reserved for future standard use");
     else if (scause == 12)
-        return("Instruction page fault");
+        return ("Instruction page fault");
     else if (scause == 13)
-        return("Load page fault");
+        return ("Load page fault");
     else if (scause == 14)
-        return("Reserved for future standard use");
+        return ("Reserved for future standard use");
     else if (scause == 15)
-        return("Store/AMO page fault");
+        return ("Store/AMO page fault");
     else if ((scause >= 16) && (scause <= 23))
-        return("Reserved for future standard use");
+        return ("Reserved for future standard use");
     else if ((scause >= 24) && (scause <= 31))
-        return("Reserved for custom use");
+        return ("Reserved for custom use");
     else if ((scause >= 32) && (scause <= 47))
-        return("Reserved for future standard use");
+        return ("Reserved for future standard use");
     else if ((scause >= 48) && (scause <= 63))
-        return("Reserved for custom use");
+        return ("Reserved for custom use");
     else if (scause >= 64)
-        return("Reserved for future standard use");
+        return ("Reserved for future standard use");
     return "";
 }
 
@@ -136,13 +136,21 @@ void do_IRQ(struct pt_regs *regs)
 
         default:
         {
-            unsigned long spec = csr_read(sepc);
-            unsigned long stval = csr_read(stval);
-
-            printv("scause:" $(scause) "\n");
-            printv("sepc: " $(spec) "\n");
-            printv("stval: " $(stval) "\n");
-            printv("Exception Description: " $(get_scause_info(scause)) "\n");
+            printv("\e[31mException Description: " $(get_scause_info(scause)) "\n");
+            printv("sepc: " $(regs->sepc) "\n");
+            printv("ra: " $(regs->ra) "\n");
+            printv("sp: " $(regs->sp) "\n");
+            printv("gp: " $(regs->gp) "\n");
+            printv("tp: " $(regs->tp) "\n");
+            printv("t0: " $(regs->t0) " t1: " $(regs->t1) " t2: " $(regs->t2) "\n");
+            printv("s0: " $(regs->s0) " s1: " $(regs->s1) "\n");
+            printv("a0: " $(regs->a0) " a1: " $(regs->a1) " a2: " $(regs->a2) " a3: " $(regs->a3) "\n");
+            printv("a4: " $(regs->a4) " a5: " $(regs->a5) " a6: " $(regs->a6) " a7: " $(regs->a7) "\n");
+            printv("s2: " $(regs->s2) " s3: " $(regs->s3) " s4: " $(regs->s4) " s5: " $(regs->s5) "\n");
+            printv("s6: " $(regs->s6) " s7: " $(regs->s7) " s8: " $(regs->s8) " s9: " $(regs->s9) "\n");
+            printv("s10: " $(regs->s10) " s11: " $(regs->s11) "\n");
+            printv("t3: " $(regs->t3) " t4: " $(regs->t4) " t5: " $(regs->t5) " t6: " $(regs->t6) "\n");
+            printv("sstatus: " $(regs->sstatus) " sbadaddr: " $(regs->sbadaddr) " scause: " $(regs->scause) "\n");
             while (1)
                 ;
             break;
